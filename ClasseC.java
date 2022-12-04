3996684614dc2f3522b2138b431e20d2d5f84e56
Lucas Malachias Furtado <lucasmfurtado@gmail.com>
2022-12-04 15:00:40 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,20 @@
+public class ClasseC {
+    private String C1;
+    private int C2;
+
+    public String getC1(){
+        return C1;
+    }
+
+    public int getC2(){
+        return C2;
+    }
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+
+    public void MC2(){
+        System.out.println("MC2");
+    }
+}
