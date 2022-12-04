5536243cfce9e5e1cb921a0217298694c807de78
Lucas <108813803+lucasmalachias@users.noreply.github.com>
2022-12-04 14:21:37 -0300

    Create ClasseA.java

---
+++ ClasseA.java
@@ -0,0 +1,28 @@
+public class ClasseA{
+  private int A1;
+  private float A2;
+  
+  public int getA1(){
+    return A1;
+   }
+   
+   public float getA2(){
+    return A2;
+   }
+   
+   public void setA1(int A){
+    A1 = A;
+   }
+   
+   public void setA2(float A){
+    A2 = A;
+   }
+   
+   public void MA1(){
+    System.out.println("MA1");
+   }
+   
+   public void MA2(){
+    System.out.println("MA2");
+   }
+}
