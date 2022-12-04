a1997f1a7deba6825f105816528fb6437362ce6f
Lucas <108813803+lucasmalachias@users.noreply.github.com>
2022-12-04 14:23:07 -0300

    Create ClasseB.java

---
+++ ClasseB.java
@@ -0,0 +1,28 @@
+public class ClasseB{
+  private int B1;
+  private float B2;
+  
+  public int getB1(){
+    return B1;
+   }
+   
+   public float getB2(){
+    return B2;
+   }
+   
+   public void setB1(int B){
+    B1 = B;
+   }
+   
+   public void setB2(float B){
+    B2 = B;
+   }
+   
+   public void MB1(){
+    System.out.println("MB1");
+   }
+   
+   public void MB2(){
+    System.out.println("MB2");
+   }
+}
