034e0cfc88ee25270ae9f743db27482e64aa818c
Lucas Malachias Furtado <lucasmfurtado@gmail.com>
2022-12-04 21:50:52 -0300

    Classes B e C modificadas. Classe D adicionada.

--- ClasseC.java
+++ ClasseC.java
@@ -17,4 +17,8 @@ public class ClasseC {
     public void MC2(){
         System.out.println("MC2");
     }
+
+    public void MC3(){
+        System.out.println("MC3");
+    }
 }
---
+++ ClasseD.java
@@ -0,0 +1,20 @@
+public class ClasseD {
+    private int D1;
+    private double D2;
+
+    public int getD1(){
+        return D1;
+    }
+
+    public double getD2(){
+        return D2;
+    }
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
